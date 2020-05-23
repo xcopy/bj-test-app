7e0f98b3126dfdad179962097687d38204b4d83b
Kairat Jenishev <kairat.jenishev@gmail.com>
2020-05-23 13:32:00 +0600

    Add pagination

--- public/index.php
+++ public/index.php
@@ -4,14 +4,20 @@ require_once dirname(__DIR__).'/vendor/autoload.php';
 require_once dirname(__DIR__).'/db/generated-conf/config.php';
 
 use Klein\Klein;
+use Klein\Request;
 use App\View;
 use App\Models\TaskQuery;
 
 $klein = new Klein();
 
-$klein->respond('GET', '/', function () {
+$klein->respond('GET', '/', function (Request $request) {
+    $page = $request->param('page') ?? 1;
+
+    $pager = TaskQuery::create()
+        ->paginate($page, $maxPerPage = 3);
+
     return View::render('index.twig', [
-        'tasks' => TaskQuery::create()->find()
+        'pager' => $pager
     ]);
 });
 
